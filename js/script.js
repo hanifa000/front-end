62cffb1720f52e81fc922d4c888e246abc5fe4d8
hanifa000 <hanifasalsabila01012001@gmail.com>
2024-05-24 05:54:06 +0700

    modifikasi jam 05.54

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+function openTab(evt, tabName) {
+  // Ambil semua elemen dengan class "tabcontent" dan sembunyikan mereka
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Ambil semua elemen dengan class "tablinks" dan hapus class "active" dari mereka
+  var tablinks = document.getElementsByClassName("tablinks");
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Tampilkan tab yang dipilih dan tandai tombol sebagai aktif
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Buka tab pertama secara default saat halaman dimuat
+document.getElementById("defaultOpen").click();
+
